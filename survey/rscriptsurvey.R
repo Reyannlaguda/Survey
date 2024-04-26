b468f88dd99827a395f4a3fa158518b7ff6dae61
caliqueth <miko.pama@students.isatu.edu.ph>
2024-04-26 06:56:43 +0000

    Good day Ma'am!

---
+++ survey/rscriptsurvey.R
@@ -0,0 +1,21 @@
+library(readxl)
+library(dplyr)
+
+responses <- read_excel("SURVEY CS102/responses.xlsx")
+responses
+
+responses$Age <- factor(responses$Age)
+responses$Sex <- factor(responses$Sex)
+responses$School <-factor(responses$School)
+responses$Course <-factor(responses$Course)
+
+responses$Age
+responses$Sex
+responses$School
+responses$Course
+
+barplot(table(responses$Age), main = "Age")
+pie(table(responses$Sex), main = "Sex")
+barplot(table(responses$School), main = "School")
+
+
